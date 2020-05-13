578b8926970d7e9fc312567136e068601757a214
Fredy <desaMts1806@MacBook-Pro-de-macpro-4.local>
2020-05-12 23:05:37 -0500

    Ciclo For en GO

---
+++ Ejercicios/Ciclos/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// i := 1
+	// for basico
+	/*for i < 10 {
+		fmt.Println(i)
+		i++
+	}*/
+
+	// for tradicional
+	// fmt.Println("For Tradicional")
+	// for i := 1; i < 10; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// For usando break
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+}
